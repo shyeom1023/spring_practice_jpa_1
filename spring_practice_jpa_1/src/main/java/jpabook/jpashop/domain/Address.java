51a056288ce6bd4c87680e9b5e60861998dca4e9
shyeom1023 <shyeom1023@gmail.com>
2021-06-08 18:09:17 +0900

    엔티티 구축

---
+++ spring_practice_jpa_1/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Embeddable;
+
+import lombok.Getter;
+
+@Getter
+@Embeddable
+public class Address {
+
+	private String city;
+	private String street;
+	private String zipcode;
+
+	protected Address() {
+
+	}
+
+	public Address(String city, String street, String zipcode) {
+		super();
+		this.city = city;
+		this.street = street;
+		this.zipcode = zipcode;
+	}
+
+}
---
+++ spring_practice_jpa_1/src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain.item;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@DiscriminatorValue("A")
+@Entity
+public class Album extends Item {
+
+	private String artist;
+	private String etc;
+
+}
